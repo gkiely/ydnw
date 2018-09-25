f3abd8a37a7285979aa1a85a1f842fac9aae5980
Pat Walls <patrickswalls@gmail.com>
2018-09-25 08:29:34 -0400

    google approved

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    pathy = current_user.email.include?("google.com") ? google_drive_authorize_path : waitlist_path
-    redirect_to pathy if current_user.google_auth_token.nil?
+    redirect_to google_drive_authorize_path if current_user.google_auth_token.nil?
     @posts = current_user.posts
   end
 end
