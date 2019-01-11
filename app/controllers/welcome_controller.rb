ff42f0ac4f3b19326bb0c6db811e47ca0464e1c2
Pat Walls <patrickswalls@gmail.com>
2019-01-12 03:27:46 +0700

    update open page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,12 +12,16 @@ class WelcomeController < ApplicationController
   end
 
   def open
-    @users_added = User.group("date_trunc('day', created_at) ").count.sort {|a,b| a[0] <=> b[0]}
-    @days = @users_added.map { |user| user[0].strftime("%d %B %Y") }
+    @users_added = User.where.not(google_auth_token: nil).group("date_trunc('month', created_at) ").count.sort {|a,b| a[0] <=> b[0]}
+    @days = @users_added.map { |user| user[0].strftime("%B %Y") }
     @values = @users_added.map { |user| user[1] }
 
-    @authed_users_added = User.where.not(google_auth_token: nil).group("date_trunc('day', created_at) ").count.sort {|a,b| a[0] <=> b[0]}
-    # @days = @users_added.map { |user| user[0].strftime("%d %B %Y") }
-    @authed_values = @authed_users_added.map { |user| user[1] }
+    @posts_added = Post.group("date_trunc('month', created_at) ").count.sort {|a,b| a[0] <=> b[0]}
+    @posts_days = @posts_added.map { |post| post[0].strftime("%B %Y") }
+    @posts_values = @posts_added.map { |post| post[1] }
+
+    @published_posts_added = Post.where(is_published: true).group("date_trunc('month', created_at) ").count.sort {|a,b| a[0] <=> b[0]}
+    @published_posts_days = @published_posts_added.map { |post| post[0].strftime("%B %Y") }
+    @published_posts_values = @published_posts_added.map { |post| post[1] }
   end
 end
